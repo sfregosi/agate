9359f20c28593e3460e5bbeb9a2efa0257a7401b
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-05-18 17:40:34 -0400

    turn mapping contours for SOCAL2020 into shapefiles for deliverables;

--- mapping/contoursToShapefile.m
+++ mapping/contoursToShapefile.m
@@ -1,6 +1,12 @@
-function contoursToShapefile(C)
+function shp = contoursToShapefile(c)
 
 % export contour lines as shapefile for deliverables
+
+% from file exchange - reshape contours into different pieces
+[x,y,z] = C2xyz(c);
+% save as shp structure
+shp = struct('Geometry', 'Line', 'Lon', x, 'Lat', y, 'Z', num2cell(z));
+
 
 
 
