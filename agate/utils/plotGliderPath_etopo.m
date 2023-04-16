faf55dbbee4de3688c88451cdbe8de12d2ac9b55
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-04-15 21:43:44 -0700

    now specify bathy file in function call

--- agate/utils/plotGliderPath_etopo.m
+++ agate/utils/plotGliderPath_etopo.m
@@ -1,4 +1,4 @@
-function plotGliderPath_etopo(CONFIG, pp, targetsFile)
+function plotGliderPath_etopo(CONFIG, pp, targetsFile, bathyFile)
 % PLOTGLIDERPATH_ETOPO	Plot glider path on bathymetric map
 %
 %	Syntax:
@@ -13,26 +13,18 @@ function plotGliderPath_etopo(CONFIG, pp, targetsFile)
 %
 %	Inputs:
 %		CONFIG      Mission/agate global configuration variable
-%       pp          Piloting parameters table created with 
+%       pp          Piloting parameters table created with
 %                   extractPilotingParams.m
-%       A0_24V      Value of the $A0_24V parameter with total available amp
-%                   hours for this glider (e.g., 310 for 15V system)
-%       tmd         target mission duration in days
+%       targetsFile Fullfile reference to the text file targetsFile
+%       bathyFile   Optional argument to plot bathymetry (slow step),
+%                   either specify the fullfile (CONFIG.map.bathyFile) or 
+%                   set to 0 to not plot bathymetry
 %
-
-% Inputs:
-% CONFIG        deployment parameters - glider serial, deployment string, pmcard
-% pp            piloting parameters table created w extractPilotingParams.m
-% PATHS         path to shape files on workstation set in setWorkstation.m
-% path_bsLocal  location of locally saved basetation files
-% path_status   output directory to save status plots and tables
-
-
 %	Outputs:
 %		no output, creates figure
 %
 %	Examples:
-%       plotBattUseFreeSpace(CONFIG, pp639, 310, 60)
+%       plotBattUseFreeSpace(CONFIG, pp639, 310, CONFIG.tmd)
 %	See also
 %       extractPilotingParams
 %
@@ -41,11 +33,17 @@ function plotGliderPath_etopo(CONFIG, pp, targetsFile)
 %	Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 %
 %	FirstVersion: 	13 April 2023
-%	Updated:
+%	Updated:        15 April 2023
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if nargin < 4
+    bathyOn = 0; % if no file specified, don't plot bathymetry
+elseif ~isempty(bathyFile)
+    bathyOn = 1;
+end
+
 % set fig number so you can place it in one spot on your desktop and not
-% have to keep resizing, moving, etc. 
+% have to keep resizing, moving, etc.
 figNum = CONFIG.figNumList(4);
 mapFigPosition = [100    100    800    600];
 
@@ -71,55 +69,69 @@ mlabel('MLabelLocation', 1, 'MLabelRound', -1, ...
     'MLabelParallel', 'south', 'FontSize', 14);
 tightmap
 
-% ***NEED TO MANUALLY PLACE NORTH ARROW AND SCALE BAR***
-% na = northarrow('latitude', 32.9, 'longitude', -121);
-% scaleruler on
-% % showaxes
-% setm(handlem('scaleruler1'), 'RulerStyle', 'patches', ...
-%     'XLoc', 0.005, 'YLoc', 0.567, 'MajorTick', [0:50:100], ...
-%     'MinorTick', [0:12.5:25], 'FontSize', 14);
-
-% geoshow(states, 'FaceColor', [0 0 0], 'EdgeColor', 'k')
+% add north arrow and scale bar
+CONFIG.map.na = northarrow('latitude', CONFIG.map.naLat, 'longitude', ...
+    CONFIG.map.naLon, 'FaceColor', [1 1 1], 'EdgeColor', [1 1 1]);
+scaleruler on
+% showaxes
+setm(handlem('scaleruler1'), 'RulerStyle', 'patches', ...
+    'XLoc', CONFIG.map.scalePos(1), 'YLoc', CONFIG.map.scalePos(2), ...
+    'MajorTick', CONFIG.map.scaleMajor, 'MinorTick', CONFIG.map.scaleMinor, ...
+    'FontSize', 14);
 
 %%  plot bathymetry - slow step
-[Z, refvec] = etopo([CONFIG.path.shp 'etopo1\etopo1_ice_c_i2.bin'], 1, ...
-    CONFIG.map.latLim, CONFIG.map.lonLim);
+%  plot bathymetry - slow step - optional
+if bathyOn
+    % try the specified file
+%     bathyFile = fullfile(CONFIG.path.shp, 'etopo2022', ...
+%         'ETOPO_2022_v1_60s_N90W180_surface.tif');
+    % if that's no good, prompt to select correct file
+    if ~exist(bathyFile, 'file')
+        [fn, path] = uigetfile(fullfile(CONFIG.path.shp, '*.tif;*.tiff'), ...
+            'Select etopo raster file');
+        bathyFile = fullfile(path, fn);
+    end
+    [Z, refvec] = readgeoraster(bathyFile, 'OutputType', 'double', ...
+        'CoordinateSystemType', 'geographic');
+    [Z, refvec] = geocrop(Z, refvec, CONFIG.map.latLim, CONFIG.map.lonLim);
 
-Z(Z >= 1) = NaN;
-geoshow(Z, refvec, 'DisplayType', 'surface', ...
-    'ZData', zeros(size(Z)), 'CData', Z);
-cmap = cmocean('ice');
-cmap = cmap(150:256,:);
-colormap(cmap)
-caxis([-6000 0])
-brighten(.4);
+    Z(Z >= 10) = 100;
+    geoshow(Z, refvec, 'DisplayType', 'surface', ...
+        'ZData', zeros(size(Z)), 'CData', Z);
 
-[c,h] = contourm(Z, refvec, [-5000:1000:1000], 'LineColor', [0.6 0.6 0.6]);
-[c,h] = contourm(Z, refvec, [-1000 -1000], 'LineColor', [0.3 0.3 0.3], 'LineWidth', 0.8);
-[c,h] = contourm(Z, refvec, [-900:100:0], 'LineColor', [0.8 0.8 0.8]);
-[c,h] = contourm(Z, refvec, [-500 -500], 'LineColor', [0.6 0.6 0.6]);
+    cmap = cmocean('ice');
+    cmap = cmap(150:256,:);
+    colormap(cmap)
+    clim([-6000 0])
+    brighten(.4);
 
+    [c,h] = contourm(Z, refvec, [-5000:1000:1000], 'LineColor', [0.6 0.6 0.6]);
+    [c,h] = contourm(Z, refvec, [-1000 -1000], 'LineColor', [0.3 0.3 0.3], ...
+        'LineWidth', 0.8);
+    [c,h] = contourm(Z, refvec, [-900:100:0], 'LineColor', [0.8 0.8 0.8]);
+    [c,h] = contourm(Z, refvec, [-500 -500], 'LineColor', [0.6 0.6 0.6]);
+end
 geoshow(states, 'FaceColor', [0 0 0], 'EdgeColor', 'k')
 
 
 %% plot waypoints
-plotm(targets.lat, targets.lon, 'Marker', 'o', 'MarkerSize', 4, 'MarkerEdgeColor', [0 0 0], ...
-    'MarkerFaceColor', [0 0 0], 'Color', [0 0 0])
+plotm(targets.lat, targets.lon, 'Marker', 'o', 'MarkerSize', 4, ...
+    'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0])
 textm(targets.lat, targets.lon, targets.name, 'FontSize', 10)
 
 %% plot any landmarks
 
 % % plot rPo4
 % plotm(33.228783, -118.251487, 'Marker', 'o', 'MarkerSize', 4, 'MarkerEdgeColor', [0 0 0], ...
-%     'MarkerFaceColor', [0 0 0], 'Color', [0 0 0]) 
+%     'MarkerFaceColor', [0 0 0], 'Color', [0 0 0])
 % textm(33.228783, -118.251487,'RPo4')
-% 
+%
 % % % plot HARP N from Simone
 % % plotm(32.3696, -118.5653, 'Marker', 'o', 'MarkerSize', 4, 'MarkerEdgeColor', [0 0 0], ...
-% %     'MarkerFaceColor', [0 0 0], 'Color', [0 0 0]) 
+% %     'MarkerFaceColor', [0 0 0], 'Color', [0 0 0])
 % % textm(32.3696, -118.5653,'HARP-N')
-% 
-% 
+%
+%
 % % plot san pedro
 % plotm(33.74, -118.29, 'Marker', 'p', 'MarkerSize', 4, 'MarkerEdgeColor', [1 1 1], ...
 %     'MarkerFaceColor', [1 1 1], 'Color', [1 1 1])
@@ -133,11 +145,12 @@ time = nan(height(pp)*2,1);
 per = nan(height(pp)*2,1);
 for d = 1:height(pp)
     try
-    lat((d*2-1):d*2) = [pp.startGPS{d}(1); pp.endGPS{d}(1)];
-    lon((d*2-1):d*2) = [pp.startGPS{d}(2); pp.endGPS{d}(2)];
-    dive((d*2-1):d*2) = [d; d];
-    time((d*2-1):d*2) = [datenum(pp.diveStartTime(d)); datenum(pp.diveEndTime(d))];
-    per((d*2-1):d*2) = [1, 2]; 
+        lat((d*2-1):d*2) = [pp.startGPS{d}(1); pp.endGPS{d}(1)];
+        lon((d*2-1):d*2) = [pp.startGPS{d}(2); pp.endGPS{d}(2)];
+        dive((d*2-1):d*2) = [d; d];
+        time((d*2-1):d*2) = [datenum(pp.diveStartTime(d)); 
+            datenum(pp.diveEndTime(d))];
+        per((d*2-1):d*2) = [1, 2];
     catch
         continue
     end
@@ -153,18 +166,19 @@ textm(lat(end), lon(end), num2str(height(pp)));
 scale = 5;
 
 endLat = NaN(height(pp),1);
-endLon = NaN(height(pp),1); 
+endLon = NaN(height(pp),1);
 for d = 1:height(pp)
     try
-    endLat(d) = pp.endGPS{d}(1);
-    endLon(d) = pp.endGPS{d}(2);
+        endLat(d) = pp.endGPS{d}(1);
+        endLon(d) = pp.endGPS{d}(2);
     catch
         continue
     end
 end
-quiverm(endLat(logical(~isnan(pp.dac_east_cm_s))), endLon(logical(~isnan(pp.dac_east_cm_s))),...
-    pp.dac_north_cm_s(logical(~isnan(pp.dac_east_cm_s))), pp.dac_east_cm_s(logical(~isnan(pp.dac_east_cm_s))), ...
-    'b')
+quiverm(endLat(logical(~isnan(pp.dac_east_cm_s))), ...
+    endLon(logical(~isnan(pp.dac_east_cm_s))),...
+    pp.dac_north_cm_s(logical(~isnan(pp.dac_east_cm_s))), ...
+    pp.dac_east_cm_s(logical(~isnan(pp.dac_east_cm_s))), 'b')
 
 
 end
