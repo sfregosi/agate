add40985aaeb3b5267edd3138b2f68cd5766115e
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-04-23 16:25:33 -0700

    updated to work with CONFIG, better documentation
    also does not automatically save now

--- agate/utils/plotSoundSpeedProfile.m
+++ agate/utils/plotSoundSpeedProfile.m
@@ -1,32 +1,42 @@
-function plotSoundSpeedProfile(locCalcT, savePath)
-% read in glider locCalc file and create a plot of sound speed.
-% plot mean and median thicker on top of profiles for every individual dive
+function plotSoundSpeedProfile(CONFIG, locCalcT)
+% PLOTSOUNDSPEEDPROFILE	Plot sound speed profile
+%
+%	Syntax:
+%		PLOTSOUNDSPEEDPROFILE(LOCCALCT)
+%
+%	Description:
+%		Read in glider locCalcT data variable and create a plot of sound
+%       speed with individual sound speed profiles for every dive in grey
+%       and the mean and median thicker and in black over the top.
+%
+%	Inputs:
+%       CONFIG      global agate mission configuration variable (for plot
+%                   titles)
+%		locCalcT    matlab object with table of glider sensor data
+%                   extracted with extractPositionalData. Must contain
+%                   columns for soundVelocity and depth
+%
+%	Outputs:
+%		no output, creates figure
+%
+%	Examples:
+%
+%	See also
+%       extractPositionalData
+%
+%	Authors:
+%		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%	Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
+%
+%	FirstVersion: 	12 November 2020 (last known date)
+%	Updated:
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% inputs:
-% locCalcT: .mat file (with full path) with table of glider sensor data
-% extracted using positionalDataExtractor - needs to have cols for
-% soundVelocity and depth
-% if file not specified, will prompt for file
-% savePath: path to save .fig and .png. leave empty to not save
-% automatically
-% If saving, will prompt input for figure title in Command Window
-
-% S. Fregosi updated 2020/11/12
-
-if nargin < 2
-    savePath = [];
-end
-
-if nargin < 1
-    [file_in, path_in] = uigetfile('*.mat', 'Select locCalcT file');
-    % path_in = ['G:\score\2015\profiles\' gldr '_' lctn '_' dplymnt '\'];
-    % load([file_in gldr '_' lctn '_' dplymnt '_locCalcT_pam.mat']);
-    
-    load([path_in file_in]);
-end
+global CONFIG
 
 figure;
-plot(locCalcT.soundVelocity,-locCalcT.depth,'Color',[0.8 0.8 0.8], 'HandleVisibility', 'off')
+plot(locCalcT.soundVelocity, -locCalcT.depth, 'Color', [0.8 0.8 0.8], ...
+    'HandleVisibility', 'off')
 hold on
 ylim([-1010 10]);
 xlabel('sound speed [m/s]')
@@ -45,8 +55,8 @@ for d = 0:res:1000 % to 1000 m
     meanSSP(b,1) = -d;
     medianSSP(b,1) = -d;
     [r, ~] =  find(locCalcT.depth >= d & locCalcT.depth < (d + res));
-    meanTmp = nanmean(locCalcT.soundVelocity(r));
-    medianTmp = nanmedian(locCalcT.soundVelocity(r));
+    meanTmp = mean(locCalcT.soundVelocity(r), 'omitnan');
+    medianTmp = median(locCalcT.soundVelocity(r), 'omitnan');
     meanSSP(b,2) = meanTmp;
     medianSSP(b,2) = medianTmp;
 end
@@ -55,11 +65,8 @@ plot(meanSSP(:,2), meanSSP(:,1), 'k','LineWidth',2)
 plot(medianSSP(:,2), medianSSP(:,1), 'k--', 'LineWidth',2)
 
 legend('mean', 'median', 'Location', 'southeast')
-if ~isempty(savePath)
-    titleString = input('Figure title string: ','s');
-    title(titleString,'Interpreter','none');
-    print([savePath titleString '_SSP.png'],'-dpng')
-    savefig([savePath titleString '_SSP.fig'])
-end
+
+title(['Sound Speed Profile: ' CONFIG.glider CONFIG.mission], ...
+    'Interpreter','none');
 
 end
