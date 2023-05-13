883b675c8783ad618bec69b54171cfafc4b359af
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-05-12 17:28:52 -0700

    fixed date of ver

--- agate/agate.m
+++ agate/agate.m
@@ -23,10 +23,11 @@ function agate(missionCnf)
 %
 %   Authors:
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
-%       Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 %
 %   FirstVersion:   06 April 2023
-%   Updated:        14 April 2023
+%   Updated:        12 May 2023
+%
+%   Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear global;  % clear out old globals
