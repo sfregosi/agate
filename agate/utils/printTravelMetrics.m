f801c8b4371e2c942364757ce35e4ad786e6f73e
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-05-27 12:44:10 -0700

    trimmed some out that is now in printRecoveryMetrics
    + rearrange some processing steps and rounded eta's to nearest hour

--- agate/utils/printTravelMetrics.m
+++ agate/utils/printTravelMetrics.m
@@ -2,7 +2,7 @@ function tm = printTravelMetrics(CONFIG, pp, targetsFile, printOn)
 %PRINTTRAVELMETRICS	Print out summary of distances/days traveled/remaining
 %
 %   Syntax:
-%      PRINTTRAVELMETRICS(CONFIG, pp, targetsFile)
+%     tm = PRINTTRAVELMETRICS(CONFIG, pp, targetsFile)
 %
 %   Description:
 %        Summarize and print out several metrics about mission distances
@@ -22,82 +22,90 @@ function tm = printTravelMetrics(CONFIG, pp, targetsFile, printOn)
 %                   default is to print
 %
 %   Outputs:
-%       tm          structure of travel metrics includes 
-%                   distTot     total distance over ground in km
-%                   distCov     total distance along trackline in km
-%                   distRem     trackline distance remaining in km
-%                   missionDur  total mission duration in days
-%                   avgSpd      average speed over ground in km/day
-%                   avgTrkSpd   average speed along trackline in km/day
-%                   daysRem     estimated days remaining to finish trackline
+%       tm   [struct] of calculated/estimated travel metrics, includes 
+%               distTot         total distance over ground in km
+%               distCov         total distance along trackline in km
+%               distRem         trackline distance remaining in km
+%               missionElapsed  total mission duration in days
+%               avgSpd          average speed over ground in km/day
+%               avgTrkSpd       average speed along trackline in km/day
+%               daysRem         estimated days remaining to finish trackline
 %       also prints out lines of text
 %
 %   Examples:
 %       tm = printTravelMetrics(CONFIG, pp, targetsFile, 1);
 %
-%   See also
+%   See also PRINTRECOVERYMETRICS
 %
 %
 %   Authors:
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
 %   FirstVersion:   25 April 2023
-%   Updated:        25 May 2023
+%   Updated:        27 May 2023
 %
 %   Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if nargin < 4
+	printOn = 1;
+end
+
 tm = struct;
 
-% dist over ground
+% total dist over ground
 tm.distTot = sum(pp.dog_km(~isnan(pp.dog_km)));
+% total elapsed days
+tm.missionElapsed = days(pp.diveEndTime(end) - pp.diveStartTime(1));
 
-% distance covered and remaining
-% get current waypoint and use previous for trackline covered calcs
+% estimate track distance covered and remaining
+% loop through all targets (expect RECV) to get distances between each
 [targets, ~] = readTargetsFile(CONFIG, targetsFile);
-currTgt = pp.tgtName{end};
-ctIdx = find(strcmp(targets.name, currTgt));
-% loop through all targets (expect RECV) to get distances
 for f = 1:height(targets) - 1
     [targets.distToNext_km(f), ~] = lldistkm([targets.lat(f+1) targets.lon(f+1)], ...
         [targets.lat(f) targets.lon(f)]);
 end
-% sum
-dist_remEst = sum(targets.distToNext_km(ctIdx:end));
-tm.distRem = dist_remEst + pp.distTGT_km(end);
+% get current waypoint to calculate distance covered and remaining
+currTgt = pp.tgtName{end};
+ctIdx = find(strcmp(targets.name, currTgt));
+% sum dists between all waypoints before current waypoint, then subtract
+% remaining distance to currant waypoint to get total trackline covered
 dist_covEst = sum(targets.distToNext_km(1:ctIdx-1));
 tm.distCov = dist_covEst - pp.distTGT_km(end);
-
-% days
-tm.missionDur = days(pp.diveEndTime(end) - pp.diveStartTime(1));
+% sum dist between all remaining waypoints + the remaining dist to current
+% targe tto get total trackline remaining
+dist_remEst = sum(targets.distToNext_km(ctIdx:end));
+tm.distRem = dist_remEst + pp.distTGT_km(end);
 
 % speeds
-tm.avgSpd = tm.distTot/tm.missionDur;
-tm.avgTrkSpd = tm.distCov/tm.missionDur;
+tm.avgSpd = tm.distTot/tm.missionElapsed;
+tm.avgTrkSpd = tm.distCov/tm.missionElapsed;
+% last 5 dives only (typically 20-30 hours)
+tm.avgSpdRec = sum(pp.dog_km(end-4:end))/...
+	days(pp.diveEndTime(end) - pp.diveStartTime(end-4));
 
 % remaining days
 tm.missionRem = tm.distRem/tm.avgTrkSpd;
+tm.missionRemRec = tm.distRem/tm.avgSpdRec;
 
-% eta
-tm.eta = datetime('now', 'Format', 'uuuu-MMM-dd HH:mm:ss ZZZZ', ...
-	'TimeZone', '+0000') + days(tm.missionRem);
+% eta to recovery 
+tm.eta = dateshift(datetime(pp.diveEndTime(end), 'Format', 'uuuu-MMM-dd HH:mm ZZZZ', ...
+	'TimeZone', '+0000') + days(tm.missionRem), 'start', 'hour', 'nearest');
+tm.etaRec = dateshift(datetime(pp.diveEndTime(end), 'Format', 'uuuu-MMM-dd HH ZZZZ', ...
+	'TimeZone', '+0000') + days(tm.missionRemRec), 'start', 'hour', 'nearest');
 
-% avg speed over last 5 dives (which is typically around 24-30 hours
-tm.avgSpdRec = sum(pp.dog_km(end-4:end))/...
-	days(pp.diveEndTime(end) - pp.diveStartTime(end-4));
 
 if printOn == 1
     % print messagess
     fprintf(1, ['%s travel summary through dive %i:\n' ...
-		'\tTotal distance over ground: %.1f km  (~%.f km of trackline) ' ...
+		'\tTotal distance over ground: %.1f km (~%.f km of trackline) ' ...
         'in %.1f days.\n' ...
-        '\tAvg speed over ground: %.f km/day. Avg speed over trackline: ~%.f km/day.\n' ...
-		'\tAvg speed over ground, last 5 dives: %.f km/day.\n'...
-        '\tApprox. %.f days to complete mission (total ~= %.f days, target = %i days).\n'...
-		'\t\t***ETA is %s***\n'], ...
-		CONFIG.glider, pp.diveNum(end), tm.distTot, tm.distCov, tm.missionDur, ...
-		tm.avgSpd, tm.avgTrkSpd, tm.avgSpdRec, tm.missionRem, tm.missionRem + tm.missionDur, ...
-		CONFIG.tmd, tm.eta);
+        '\tAvg speed over ground: %.f km/day (last 5 dives only = %.f km/day)\n' ...
+		'\tAvg speed over trackline: ~%.f km/day.\n' ...
+        '\tEstimated mission duration: %.f days (target = %.f days)\n'], ...
+		CONFIG.glider, pp.diveNum(end), tm.distTot, tm.distCov, ...
+		tm.missionElapsed, tm.avgSpd, tm.avgSpdRec, tm.avgTrkSpd, ...
+		tm.missionRem + tm.missionElapsed, CONFIG.tmd);
 end
 
 end
