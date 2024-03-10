6b89de3d440b79b5fc92b9996bab7f8d640aff6a
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-03-09 16:47:20 -0800

    simplified example

--- agate/example_workflows/workflow_processPositionalData.m
+++ agate/example_workflows/workflow_processPositionalData.m
@@ -19,14 +19,15 @@
 %
 %	Authors:
 %		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
-%	Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 %
 %	FirstVersion: 	21 April 2023
-%	Updated:        23 April 2023
+%	Updated:        09 March 2024
+%
+%	Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % initialize agate
-agate agate_config_sg639_MHI_Apr2023.cnf
+agate agate_mission_config.cnf
 global CONFIG
 
 %% extract positional data
