7e91510be13585ddcb6baab5497f706e518d4a7a
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-03-09 16:47:29 -0800

    started scratch versions

---
+++ agate/scratch/workflow_processCetaceanEncounters.m
@@ -0,0 +1,19 @@
+% WORKFLOW_PROCESSCETACEANENCOUNTERS.M
+%	One-line description here, please
+%
+%	Description:
+%		Detailed description here, please
+%
+%	Notes
+%
+%	See also
+%
+%
+%	Authors:
+%		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%
+%	FirstVersion: 	09 March 2024
+%	Updated:
+%
+%    Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
