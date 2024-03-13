187169a55e79378001dcb5f9d4a68bc3b855f779
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-03-13 13:20:24 -0700

    created for future dev

---
+++ agate/scratch/workflow_cleanLogs.m
@@ -0,0 +1,23 @@
+% WORKFLOW_CLEANLOGS.M
+%	Example workflow for cleaning up/combining Triton or ERMA logs
+%
+%	Description:
+%		Detailed description here, please
+%
+%	Notes
+%
+%	See also
+%
+%
+%	Authors:
+%		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%
+%	FirstVersion: 	13 March 2024
+%	Updated:
+%
+%	Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% created this placeholder but have done no work on it. 
+
+% see glider-mhi repo for example from MHI project - workflow_cleanUpLogs.m
