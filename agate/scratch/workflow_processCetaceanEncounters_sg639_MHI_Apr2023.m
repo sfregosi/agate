987cc84a66a5f6d2db1ab85956524cb95b41f6c0
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-03-14 08:15:30 -0700

    up to date with example, logs cleaned

--- agate/scratch/workflow_processCetaceanEncounters_sg639_MHI_Apr2023.m
+++ agate/scratch/workflow_processCetaceanEncounters_sg639_MHI_Apr2023.m
@@ -1,11 +1,17 @@
-% WORKFLOW_PROCESSCETACEANENCOUNTERS
+% WORKFLOW_PROCESSCETACEANENCOUNTERS_SG639_MHI_APR2023
 %	Workflow for creating summary plots and tables for cetacean encounters
 %
 %	Description:
 %		This script takes inputs of glider positional data and cetacean
 %		acoustic encounter data (identified by automated or manual methods)
-%		and pairs them up to summarize cetacean encounters and create maps
-%		of glider locations at the time of cetacean acoustic encounters
+%		and pairs them up to summarize cetacean events/encounters and 
+%       create maps of glider locations at the time of cetacean acoustic 
+%       encounters
+%
+%       Sections:
+%       (1) Cleanup event logs (either collapse Triton logs, or combine
+%           ERMA detection logs
+%       (2) Create encounter map
 %
 %	Notes
 %
@@ -16,14 +22,28 @@
 %		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
 %	FirstVersion: 	09 March 2024
-%	Updated:
+%	Updated:        14 March 2024
 %
 %    Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % initialize agate - either specify a .cnf or leave blank to browse/select
 agate secret/agate_config_sg639_MHI_Apr2023.cnf
 global CONFIG
+
+%% (1) Cleanup encounter logs
+% Triton manual pick logs
+logFileName = [CONFIG.gmStr '_MW_recheck.xls'];
+logFile = fullfile(CONFIG.path.analysis, 'triton', 'wood', ...
+	[CONFIG.gmStr '_recheck_mw'], logFileName);
+eventGap = 15;
+[tl, tlm] = collapseTritonLog(logFile, eventGap);
+% save as .mat and .csv
+save(fullfile(CONFIG.path.analysis, 'triton', 'wood', ...
+	[CONFIG.gmStr '_log_merged.mat']), 'tl', 'tlm');
+writetable(tlm, fullfile(CONFIG.path.analysis, 'triton', 'wood', ...
+	[CONFIG.gmStr '_log_merged.csv']));
+
+
 
 %% (1) Plot encounter on map
 
--- agate/scratch/workflow_processCetaceanEncounters_sg679_MHI_May2023.m
+++ agate/scratch/workflow_processCetaceanEncounters_sg679_MHI_May2023.m
@@ -1,14 +1,15 @@
-% WORKFLOW_PROCESSCETACEANENCOUNTERS
+% WORKFLOW_PROCESSCETACEANENCOUNTERS_SG679_MHI_MAY2023
 %	Workflow for creating summary plots and tables for cetacean encounters
 %
 %	Description:
 %		This script takes inputs of glider positional data and cetacean
 %		acoustic encounter data (identified by automated or manual methods)
-%		and pairs them up to summarize cetacean encounters and create maps
-%		of glider locations at the time of cetacean acoustic encounters
+%		and pairs them up to summarize cetacean events/encounters and 
+%       create maps of glider locations at the time of cetacean acoustic 
+%       encounters
 %
 %       Sections:
-%       (1) Cleanup encounter logs (either collapse Triton logs, or combine
+%       (1) Cleanup event logs (either collapse Triton logs, or combine
 %           ERMA detection logs
 %       (2) Create encounter map
 %
@@ -21,10 +22,9 @@
 %		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
 %	FirstVersion: 	09 March 2024
-%	Updated:        13 March 2024
+%	Updated:        14 March 2024
 %
 %    Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % initialize agate - either specify a .cnf or leave blank to browse/select
 agate secret/agate_config_sg679_MHI_May2023.cnf
@@ -33,15 +33,24 @@ global CONFIG
 %% (1) Cleanup encounter logs
 
 % ERMA detection logs
-% ERMA detection logs are created per dive, so need to combine them all
 ermaDets = combineErmaLogs(CONFIG.path.bsLocal, []);
+% save as .mat and .csv
+save(fullfile(CONFIG.path.analysis, ...
+	[CONFIG.gmStr '_ERMA_encounters_combined.mat']), 'ermaDets');
+writetable(ermaDets, fullfile(CONFIG.path.analysis, ...
+	[CONFIG.gmStr '_ERMA_encounters_combined.csv']));
 
-% Triton detection logs
-logFileName = [CONFIG.glider '_' CONFIG.mission '_MW.xls'];
+% Triton manual pick logs
+logFileName = [CONFIG.gmStr '_MW.xls'];
 logFile = fullfile(CONFIG.path.analysis, 'triton', 'wood', ...
-	[glider '_' mission '_log_mw'], logFileName);
+	[CONFIG.gmStr '_log_mw'], logFileName);
 eventGap = 15;
 [tl, tlm] = collapseTritonLog(logFile, eventGap);
+% save as .mat and .csv
+save(fullfile(CONFIG.path.analysis, 'triton', 'wood', ...
+	[CONFIG.gmStr '_log_merged.mat']), 'tl', 'tlm');
+writetable(tlm, fullfile(CONFIG.path.analysis, 'triton', 'wood', ...
+	[CONFIG.gmStr '_log_merged.csv']));
 
 %% (1) Create encounter map
 
