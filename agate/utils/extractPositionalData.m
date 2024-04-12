a6bcd1bcb2805410c788973e1f3adb2bb633c36e
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-04-12 12:10:57 -0700

    set up pre-allocation for speed improvements
    to fix issue #7

--- agate/utils/extractPositionalData.m
+++ agate/utils/extractPositionalData.m
@@ -41,130 +41,165 @@ function [gpsSurfT, locCalcT] = extractPositionalData(CONFIG, plotOn)
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
 %    FirstVersion:   03 September 2019
-%    Updated:        11 April 2024
+%    Updated:        12 April 2024
 %
 %    Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 % Surface locations - extracted from individual nc files
 files = dir(fullfile(CONFIG.path.mission, 'basestationFiles\p*.nc'));
+lf = length(files);
 
+% use number of files (number of dives) to preallocate
+% sometimes odd abort behaviors mess this up, but NaNs will fill in
+surfNames = {'dive', 'startTime', 'startDateTime', 'startLatitude', ...
+	'startLongitude', 'endTime', 'endDateTime', 'endLatitude', 'endLongitude', ...
+	'duration_hr', 'distance_km', 'maxDepth_m', 'dac_n', 'dac_e', ...
+	'dac_n_gsm', 'dac_e_gsm', 'dac_qc', 'avg_speed_n', 'avg_speed_e', ...
+	'avg_speed_n_gsm', 'avg_speed_e_gsm', 'hdm_qc'};
+
+gpsSurfT = array2table(NaN(lf, length(surfNames)));
+gpsSurfT.Properties.VariableNames = surfNames;
+gpsSurfT.startDateTime = datetime(gpsSurfT.startDateTime, 'ConvertFrom', 'datenum');
+gpsSurfT.endDateTime = datetime(gpsSurfT.endDateTime, 'ConvertFrom', 'datenum');
+
+% loop through each file/dive
 for f = 1:length(files)
-    fname = fullfile(CONFIG.path.mission, 'basestationFiles', files(f,1).name);
-    % ncdisp(fname,'/','min');
-    try
-        latgps=ncread(fname,'log_gps_lat');
-        longps=ncread(fname,'log_gps_lon');
-        timegps=ncread(fname,'log_gps_time');
+	fname = fullfile(CONFIG.path.mission, 'basestationFiles', files(f,1).name);
+	[~, fname_name, fname_ext] = fileparts(fname);
+	% ncdisp(fname,'/','min');
+	try
+		latgps = ncread(fname,'log_gps_lat');
+		longps = ncread(fname,'log_gps_lon');
+		timegps = ncread(fname,'log_gps_time');
 
-        dive(f,1) = f;
-        startTime(f,1) = unix2matlab(timegps(2));
-        startDateTime(f,1) = datetime(startTime(f,1),'ConvertFrom','datenum');
-        startLatitude(f,1) = latgps(2);
-        startLongitude(f,1) = longps(2);
-        endTime(f,1) = unix2matlab(timegps(3));
-        endDateTime(f,1) = datetime(endTime(f,1),'ConvertFrom','datenum');
-        endLatitude(f,1) = latgps(3);
-        endLongitude(f,1) = longps(3);
-        duration_hr(f,1) = hours(endDateTime(f,1)-startDateTime(f,1));
-		distance_km(f,1) = lldistkm([latgps(2) longps(2)], ...
-			[latgps(3) longps(3)]);
- 		maxDepth_m(f,1) = max(ncread(fname, 'depth'));
-        dac_n(f,1) = ncread(fname,'depth_avg_curr_north');
-        dac_e(f,1) = ncread(fname,'depth_avg_curr_east');
-        dac_qc(f,1) = ncread(fname,'depth_avg_curr_qc');
-        dac_n_gsm(f,1) = ncread(fname,'depth_avg_curr_north_gsm');
-        dac_e_gsm(f,1) = ncread(fname,'depth_avg_curr_east_gsm');
-        avg_speed_n(f,1) = ncread(fname,'flight_avg_speed_north');
-        avg_speed_e(f,1) = ncread(fname,'flight_avg_speed_east');
-        avg_speed_n_gsm(f,1) = ncread(fname,'flight_avg_speed_north_gsm');
-        avg_speed_e_gsm(f,1) = ncread(fname,'flight_avg_speed_east_gsm');
-        hdm_qc(f,1) = ncread(fname,'hdm_qc');
-    catch
-    end
+		gpsSurfT.dive(f)			= f;
+		gpsSurfT.startTime(f)		= unix2matlab(timegps(2));
+		gpsSurfT.startDateTime(f)	= datetime(gpsSurfT.startTime(f), ...
+										'ConvertFrom','datenum');
+		gpsSurfT.startLatitude(f)	= latgps(2);
+		gpsSurfT.startLongitude(f)	= longps(2);
+		gpsSurfT.endTime(f)			= unix2matlab(timegps(3));
+		gpsSurfT.endDateTime(f)		= datetime(gpsSurfT.endTime(f), ...
+										'ConvertFrom','datenum');
+		gpsSurfT.endLatitude(f)		= latgps(3);
+		gpsSurfT.endLongitude(f)	= longps(3);
+		gpsSurfT.duration_hr(f)		= hours(gpsSurfT.endDateTime(f) - ...
+										gpsSurfT.startDateTime(f));
+		gpsSurfT.distance_km(f)		= lldistkm([latgps(2) longps(2)], ...
+										[latgps(3) longps(3)]);
+		gpsSurfT.maxDepth_m(f)		= max(ncread(fname, 'depth'));
+		gpsSurfT.dac_n(f)			= ncread(fname,'depth_avg_curr_north');
+		gpsSurfT.dac_e(f)			= ncread(fname,'depth_avg_curr_east');
+		gpsSurfT.dac_qc(f)			= str2double(ncread(fname,'depth_avg_curr_qc'));
+		gpsSurfT.dac_n_gsm(f)		= ncread(fname,'depth_avg_curr_north_gsm');
+		gpsSurfT.dac_e_gsm(f)		= ncread(fname,'depth_avg_curr_east_gsm');
+		gpsSurfT.avg_speed_n(f)		= ncread(fname,'flight_avg_speed_north');
+		gpsSurfT.avg_speed_e(f)		= ncread(fname,'flight_avg_speed_east');
+		gpsSurfT.avg_speed_n_gsm(f)	= ncread(fname,'flight_avg_speed_north_gsm');
+		gpsSurfT.avg_speed_e_gsm(f)	= ncread(fname,'flight_avg_speed_east_gsm');
+		gpsSurfT.hdm_qc(f)			= str2double(ncread(fname,'hdm_qc'));
+	catch
+				fprintf(1, 'Problem loading %s. Skipped.\n', [fname_name fname_ext])
+	end
 end
-
-gpsSurfT = table(dive, startTime, startDateTime, startLatitude, ...
-	startLongitude, endTime, endDateTime, endLatitude, endLongitude, ...
-    duration_hr, distance_km, maxDepth_m, dac_n, dac_e, dac_n_gsm, dac_e_gsm, ...
-	dac_qc, avg_speed_n, avg_speed_e, avg_speed_n_gsm, avg_speed_e_gsm, hdm_qc);
 
 % optional check by plotting
 if plotOn
-    figure
-    plot(gpsSurfT.startLongitude,gpsSurfT.startLatitude,'.k');
-    text(gpsSurfT.startLongitude+0.002,gpsSurfT.startLatitude+0.002,num2str(gpsSurfT.dive));
+	figure
+	plot(gpsSurfT.startLongitude,gpsSurfT.startLatitude,'.k');
+	text(gpsSurfT.startLongitude+0.002,gpsSurfT.startLatitude+0.002, ...
+		num2str(gpsSurfT.dive));
 end
+
 
 % Sample locations - extracted from individual nc files
-files = dir(fullfile(CONFIG.path.mission, 'basestationFiles\p*.nc'));
 
-% set up output variables
-dive = []; time = [];
-latitude = []; longitude = []; latitude_gsm = []; longitude_gsm = [];
-north_displacement = []; east_displacement = [];
-north_displacement_gsm = []; east_displacement_gsm = [];
-depth = []; temperature = []; salinity = []; soundVelocity = []; density = [];
-vertSpeed = []; horzSpeed = []; speed = []; speed_qc = [];
-vertSpeed_gsm = []; horzSpeed_gsm = []; speed_gsm = [];
-pitch = []; glideAngle = []; glideAngle_gsm = []; heading = [];
+% guesstimate size for preallocation
+% 6 hr dives with 10 sec sampling interval (6 samples per min) == 2160 samples per dive
+initSize = 2160*lf;
 
+% set up output table
+calcNames = {'dive', 'time', 'dateTime', 'latitude', 'longitude', ...
+	'latitude_gsm', 'longitude_gsm', 'north_displacement', 'east_displacement', ...
+	'north_displacement_gsm', 'east_displacement_gsm', 'depth', 'temperature', ...
+	'salinity', 'soundVelocity', 'density', 'vertSpeed', 'horzSpeed', 'speed', ...
+	'speed_qc', 'vertSpeed_gsm', 'horzSpeed_gsm', 'speed_gsm', 'pitch', ...
+	'glideAngle', 'glideAngle_gsm', 'heading'};
+
+locCalcT = array2table(NaN(initSize, length(calcNames)));
+locCalcT.Properties.VariableNames = calcNames;
+locCalcT.dateTime = datetime(locCalcT.dateTime, 'ConvertFrom', 'datenum');
+
+% loop through all files
+lastIdx = 0;
+spaceCheck = false;
 for f = 1:length(files)
-    try
-        fname = fullfile(CONFIG.path.mission, 'basestationFiles', files(f,1).name);
-        %how many data points
-        finfo = ncinfo(fname);
-        dimMatch = strcmp({finfo.Dimensions.Name}, 'sg_data_point');
-        samples = finfo.Dimensions(dimMatch).Length;
-        dive = [dive; repmat(f, samples, 1)];
-        time = [time; unix2matlab(ncread(fname, 'time'))];
-        latitude = [latitude; ncread(fname, 'latitude')];
-        longitude = [longitude; ncread(fname, 'longitude')];
-        latitude_gsm = [latitude_gsm; ncread(fname, 'latitude_gsm')];
-        longitude_gsm = [longitude_gsm; ncread(fname, 'longitude_gsm')];
-        north_displacement = [north_displacement; ncread(fname, 'north_displacement')];
-        east_displacement = [east_displacement; ncread(fname, 'east_displacement')];
-        north_displacement_gsm = [north_displacement_gsm; ncread(fname, 'north_displacement_gsm')];
-        east_displacement_gsm = [east_displacement_gsm; ncread(fname, 'east_displacement_gsm')];
-        depth = [depth; ncread(fname, 'depth')];
-        temperature = [temperature; ncread(fname, 'temperature')];
-        salinity = [salinity; ncread(fname, 'salinity')];
-        soundVelocity = [soundVelocity; ncread(fname, 'sound_velocity')];
-        density = [density; ncread(fname, 'density')];
-        vertSpeed = [vertSpeed; ncread(fname, 'vert_speed')];
-        horzSpeed = [horzSpeed; ncread(fname, 'horz_speed')];
-        speed = [speed; ncread(fname, 'speed')];
-        speed_qc = [speed_qc; ncread(fname, 'speed_qc')];
-        vertSpeed_gsm = [vertSpeed_gsm; ncread(fname, 'vert_speed_gsm')];
-        horzSpeed_gsm = [horzSpeed_gsm; ncread(fname, 'horz_speed_gsm')];
-        speed_gsm = [speed_gsm; ncread(fname, 'speed_gsm')];
-        pitch = [pitch; ncread(fname, 'eng_pitchAng')];
-        glideAngle = [glideAngle; ncread(fname, 'glide_angle')];
-        glideAngle_gsm = [glideAngle_gsm; ncread(fname, 'glide_angle_gsm')];
-        heading = [heading; ncread(fname, 'eng_head')];
-    catch
-    end
+			fname = fullfile(CONFIG.path.mission, 'basestationFiles', files(f,1).name);
+		[~, fname_name, fname_ext] = fileparts(fname);
+	try
+		%how many data points
+		finfo = ncinfo(fname);
+		dimMatch = strcmp({finfo.Dimensions.Name}, 'sg_data_point');
+		samples = finfo.Dimensions(dimMatch).Length;
+		sampIdx = lastIdx + 1:lastIdx + samples;
+		% check for sufficient space - if not enough, pre-allocate more
+		if sampIdx(end) > height(locCalcT) && spaceCheck == false
+			fprintf(1, 'FYI More rows were needed!!\n')
+			spaceCheck = true; % only print that once...
+		end
+
+		% assign values
+		locCalcT.dive(sampIdx)					= repmat(f, samples, 1);
+		locCalcT.time(sampIdx)					= unix2matlab(ncread(fname, 'time'));
+		locCalcT.latitude(sampIdx)				= ncread(fname, 'latitude');
+		locCalcT.longitude(sampIdx)				= ncread(fname, 'longitude');
+		locCalcT.latitude_gsm(sampIdx)			= ncread(fname, 'latitude_gsm');
+		locCalcT.longitude_gsm(sampIdx)			= ncread(fname, 'longitude_gsm');
+		locCalcT.north_displacement(sampIdx)	= ncread(fname, 'north_displacement');
+		locCalcT.east_displacement(sampIdx)		= ncread(fname, 'east_displacement');
+		locCalcT.north_displacement_gsm(sampIdx) = ncread(fname, 'north_displacement_gsm');
+		locCalcT.east_displacement_gsm(sampIdx)	= ncread(fname, 'east_displacement_gsm');
+		locCalcT.depth(sampIdx)					= ncread(fname, 'depth');
+		locCalcT.temperature(sampIdx)			= ncread(fname, 'temperature');
+		locCalcT.salinity(sampIdx)				= ncread(fname, 'salinity');
+		locCalcT.soundVelocity(sampIdx)			= ncread(fname, 'sound_velocity');
+		locCalcT.density(sampIdx)				= ncread(fname, 'density');
+		locCalcT.vertSpeed(sampIdx)				= ncread(fname, 'vert_speed');
+		locCalcT.horzSpeed(sampIdx)				= ncread(fname, 'horz_speed');
+		locCalcT.speed(sampIdx)					= ncread(fname, 'speed');
+		locCalcT.speed_qc(sampIdx)				= ncread(fname, 'speed_qc');
+		locCalcT.vertSpeed_gsm(sampIdx)			= ncread(fname, 'vert_speed_gsm');
+		locCalcT.horzSpeed_gsm(sampIdx)			= ncread(fname, 'horz_speed_gsm');
+		locCalcT.speed_gsm(sampIdx)				= ncread(fname, 'speed_gsm');
+		locCalcT.pitch(sampIdx)					= ncread(fname, 'eng_pitchAng');
+		locCalcT.glideAngle(sampIdx)			= ncread(fname, 'glide_angle');
+		locCalcT.glideAngle_gsm(sampIdx)		= ncread(fname, 'glide_angle_gsm');
+		locCalcT.heading(sampIdx)				= ncread(fname, 'eng_head');
+
+		% move incrementally
+		lastIdx = lastIdx + samples;
+	catch
+		fprintf(1, 'Problem loading %s. Skipped.\n', [fname_name fname_ext])
+	end
 end
 
-dateTime = datetime(time, 'ConvertFrom', 'datenum');
-locCalcT = table(dive, time, dateTime, latitude, longitude, latitude_gsm, ...
-    longitude_gsm,north_displacement,east_displacement, north_displacement_gsm, ...
-    east_displacement_gsm, depth, temperature, salinity, soundVelocity, ...
-    density, vertSpeed, horzSpeed, speed, speed_qc, vertSpeed_gsm, ...
-    horzSpeed_gsm, speed_gsm, pitch, glideAngle, glideAngle_gsm, heading);
+% get dateTime from datenum
+locCalcT.dateTime = datetime(locCalcT.time, 'ConvertFrom', 'datenum');
+% remove extra NaNs from preallocation
+locCalcT = locCalcT(1:lastIdx,:);
 
 % check by plotting
 if plotOn
-    % dive profile
-    figure;
-    plot(locCalcT.time,-locCalcT.depth)
-    title('dive profile');
-    % compare hydro vs glide slope models
-    figure;
-    plot(locCalcT.longitude, locCalcT.latitude)
-    hold on
-    plot(locCalcT.longitude_gsm, locCalcT.latitude_gsm)
+	% dive profile
+	figure;
+	plot(locCalcT.time,-locCalcT.depth)
+	title('dive profile');
+	% compare hydro vs glide slope models
+	figure;
+	plot(locCalcT.longitude, locCalcT.latitude)
+	hold on
+	plot(locCalcT.longitude_gsm, locCalcT.latitude_gsm)
 end
 
 end
