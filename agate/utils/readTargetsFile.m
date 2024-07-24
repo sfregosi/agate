aaa600940eb9f74a325f1efa6d5a7e37096f47be
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-07-24 12:26:52 -0700

    more flexible number of arguments

--- agate/utils/readTargetsFile.m
+++ agate/utils/readTargetsFile.m
@@ -5,9 +5,9 @@ function [targets, targetsFile] = readTargetsFile(CONFIG, targetsFile)
 %       TARGETS = TREADTARGETSFILE(CONFIG, TARGETSFILE)
 %
 %   Description:
-%       Read in a Seaglider formatted targets file to a table variable. 
+%       Read in a Seaglider formatted targets file to a table variable.
 %       Fullfile name can be specified as input argument, or can be left
-%       blank to prompt to select targets file to read. 
+%       blank to prompt to select targets file to read.
 %
 %   Inputs:
 %       CONFIG       global CONFIG variable from agate mission
@@ -34,18 +34,34 @@ function [targets, targetsFile] = readTargetsFile(CONFIG, targetsFile)
 %   Created with MATLAB ver.: 9.9.0.1524771 (R2020b) Update 2
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if nargin < 2
-    [fileName, filePath] = uigetfile([CONFIG.path.mission, '\*.*'], ...
-        'Select targets file to read');
-    targetsFile = fullfile(filePath, fileName);
-    fprintf('targets file selected: %s\n', fileName);
+switch nargin
+	case 0
+		[fileName, filePath] = uigetfile('*.*', ...
+			'Select targets file to read');
+		targetsFile = fullfile(filePath, fileName);
+		fprintf('targets file selected: %s\n', fileName);
+	case 1
+		if isstruct(CONFIG)
+			if isfield(CONFIG.path, 'mission')
+				[fileName, filePath] = uigetfile([CONFIG.path.mission, '\*.*'], ...
+					'Select targets file to read');
+			else
+				[fileName, filePath] = uigetfile('*.*', ...
+					'Select targets file to read');
+			end
+			targetsFile = fullfile(filePath, fileName);
+			fprintf('targets file selected: %s\n', fileName);
+		elseif isstring(CONFIG)
+			targetsFile = CONFIG;
+		end
 end
+
 
 % check that is fullfile name
 [path, ~, ~] = fileparts(targetsFile);
 if isempty(path)
-    fprintf(1, ['No filepath specified, re-enter targetsFile argument with '...
-        'path included. Exiting\n']);
+	fprintf(1, ['No filepath specified, re-enter targetsFile argument with '...
+		'path included. Exiting\n']);
 end
 
 % read in file
@@ -61,18 +77,18 @@ numTargets = length(idxBreak);
 targets = table;
 
 for t = 1:numTargets
-    idxPeriod = regexp(x(idxBreak(t):end), '\.');
-    idxLat = regexp(x(idxBreak(t):end), 'lat=', 'once');
-    idxLon = regexp(x(idxBreak(t):end), 'lon=', 'once');
-    idxRad = regexp(x(idxBreak(t):end), 'radius=', 'once');
-    
-    if ~isempty(idxLat)
-        targets.name{t} = deblank(x(idxBreak(t):idxBreak(t) + idxLat - 2));
-        targets.lat(t) = str2num(x(idxBreak(t) + idxLat + 3:idxPeriod(1) + idxBreak(t) - 4)) ...
-            + str2num(deblank(x(idxPeriod(1) + idxBreak(t) - 3:idxBreak(t) + idxLon - 2)))/60;
-        targets.lon(t) = str2num(x(idxLon + idxBreak(t) + 3:idxPeriod(2) + idxBreak(t) - 4)) ...
-            - str2num(deblank(x(idxPeriod(2) + idxBreak(t) - 3:idxBreak(t) + idxRad - 2)))/60;
-    end
+	idxPeriod = regexp(x(idxBreak(t):end), '\.');
+	idxLat = regexp(x(idxBreak(t):end), 'lat=', 'once');
+	idxLon = regexp(x(idxBreak(t):end), 'lon=', 'once');
+	idxRad = regexp(x(idxBreak(t):end), 'radius=', 'once');
+
+	if ~isempty(idxLat)
+		targets.name{t} = deblank(x(idxBreak(t):idxBreak(t) + idxLat - 2));
+		targets.lat(t) = str2num(x(idxBreak(t) + idxLat + 3:idxPeriod(1) + idxBreak(t) - 4)) ...
+			+ str2num(deblank(x(idxPeriod(1) + idxBreak(t) - 3:idxBreak(t) + idxLon - 2)))/60;
+		targets.lon(t) = str2num(x(idxLon + idxBreak(t) + 3:idxPeriod(2) + idxBreak(t) - 4)) ...
+			- str2num(deblank(x(idxPeriod(2) + idxBreak(t) - 3:idxBreak(t) + idxRad - 2)))/60;
+	end
 end
 
 
