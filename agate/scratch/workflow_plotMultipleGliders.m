b3a3845e0ca8f14a180e5c870f3c182cb00cf6ef
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-08-08 10:48:40 -0700

    decent simple example
    needs testing

--- agate/scratch/workflow_plotMultipleGliders.m
+++ agate/scratch/workflow_plotMultipleGliders.m
@@ -1,87 +1,86 @@
 % WORKFLOW_PLOTMULTIPLEGLIDERS.M
-%	One-line description here, please
+%	Plot multiple glider paths and/or cetacean encounters on a single map
 %
 %	Description:
-%		Detailed description here, please
+%		Example script for creating a single map with multiple glider
+%		tracks. Glider labels and plotting colors are defined at the top
+%		and then a basemap is made where glider tracks or cetacean events
+%		can be added on top. A title and legend are added last. 
 %
 %	Notes
 %
-%	See also
+%	See also CREATEBASEMAP
 %
 %
 %	Authors:
 %		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
 %	FirstVersion: 	14 March 2024
-%	Updated:
+%	Updated:        08 August 2024
 %
 %	Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% (3) Create encounter map
+
+%% (1) Define glider labels and colors
 
 % set some colors
-col_pt = [1 1 1];       % planned track
-col_rt = [0 0 0];       % realized track
-% col_ce = [1 0.4 0];     % cetacean events - orange
-col_ce = [1 1 0.2];     % cetacean events - yellow
+col_sg = [1 1 0; % yellow
+	1 0.4 0];    % orange  
 
-% generate the basemap with bathymetry as figure 82, don't save .fig file
-[baseFig] = createBasemap(CONFIG, 1, 82); 
-baseFig.Position = [20    80    1200    700];
+% what should each glider be called?
+gliders = {'sg001', 'sg002'};
 
-% add original targets
-targetsFile = fullfile(CONFIG.path.mission, 'basestationFiles', 'targets');
-[targets, ~] = readTargetsFile(CONFIG, targetsFile); 
 
-h(1) = plotm(targets.lat, targets.lon, 'Marker', 's', 'MarkerSize', 4, ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_pt, 'Color', col_pt, ...
-	'DisplayName', 'planned track');
-% textm(targets.lat, targets.lon, targets.name, 'FontSize', 10)
+%% (2) Create the basemap
 
-% plot realized track
-% load surface positions
-load(fullfile(CONFIG.path.mission, 'profiles', 'sg679_MHI_May2023_gpsSurfaceTable.mat'));
+% configure agate with one of the gliders to get map settings
+CONFIG = agate('agate_mission_config_sg001.cnf');
+
+% set bathymetry or contours on or off
+bathyOn = 1; contourOn = 0;
+% north arrow, scale bar or map limits can be defined in the CONFIG file
+% specified above or manually set here
+% e.g., to set north arrow location
+CONFIG.map.naLat = 19.4;
+CONFIG.map.naLon = -159.6;
+
+% create basemap figure
+[baseFig] = createBasemap(CONFIG, bathyOn, contourOn);
+baseFig.Position = [20    80    1200    700]; % set position on screen
+
+
+%% (3) Plot first glider's surface positions
+
+% load gpsSurf table created with extractPositionalData
+load(fullfile(CONFIG.path.mission, 'profiles', ...
+	[CONFIG.gmStr '_gpsSurfaceTable.mat']));
+% plot
+h(1) = plotm(gpsSurfT.startLatitude, gpsSurfT.startLongitude, ...
+	'Color', col_sg(1,:), 'LineWidth', 1.5, 'DisplayName', gliders{1});
+
+
+%% (4) Plot second glider's surface positions
+
+% reinitilize agate with correct config file to get correct paths/strings
+CONFIG = agate('agate_mission_config_sg002.cnf');
+
+% load gpsSurf table created with extractPositionalData
+load(fullfile(CONFIG.path.mission, 'profiles', ...
+	[CONFIG.gmStr '_gpsSurfaceTable.mat']));
+% plot
 h(2) = plotm(gpsSurfT.startLatitude, gpsSurfT.startLongitude, ...
-	'Color', col_rt, 'LineWidth', 1.5, 'DisplayName', 'realized track');
+	'Color', col_sg(2,:), 'LineWidth', 1.5, 'DisplayName', gliders{2});
 
-% plot acoustic events
-h(3) = scatterm(tlm.lat, tlm.lon, 30, 'Marker', 'o', ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_ce, ...
-	'DisplayName', 'cetacean event');
+% this could be repeated for as many gliders as exist
 
-% add legend
-legend(h,  'Location', 'eastoutside', 'FontSize', 14)
-
-% add title
-title('SG679 MHI May 2023', 'Interpreter', 'none')
-
-%% (4) Add second glider to plot
-
-col_ce_sg639 = [1 0.4 0];     % cetacean events - orange
-
-% targets
-targetsFile_sg639 = 'D:\sg639_MHI_Apr2023\piloting\basestationFiles\targets';
-[targets_sg639 ,~] = readTargetsFile(CONFIG, targetsFile_sg639); 
-
-h(4) = plotm(targets_sg639.lat, targets_sg639.lon, 'Marker', 's', 'MarkerSize', 4, ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_pt, 'Color', col_pt, ...
-	'HandleVisibility', 'off');
-
-% realized track
-loadTmp = load(fullfile('D:\sg639_MHI_Apr2023\piloting\profiles\sg639_MHI_Apr2023_gpsSurfaceTable.mat'));
-gpsSurfT_sg639 = loadTmp.gpsSurfT;
-h(5) = plotm(gpsSurfT_sg639.startLatitude, gpsSurfT_sg639.startLongitude, ...
-	'Color', col_rt, 'LineWidth', 1.5, 'DisplayName', 'realized track');
-
-% plot acoustic events
-h(6) = scatterm(tlm.lat, tlm.lon, 30, 'Marker', 'o', ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', col_ce, ...
-	'DisplayName', 'cetacean event');
-
+%% (5) Add legend, title and save
 
 % add legend
-legend(h,  'Location', 'eastoutside', 'FontSize', 14)
+legend(h,  'Location', 'northeast', 'FontSize', 14)
 
 % add title
-title('SG679 MHI May 2023', 'Interpreter', 'none')
+title('Glider mission', 'Interpreter', 'none')
+
+% save it
+exportgraphics(gca, 'combined_map.png', 'Resolution', 600);
 
