e4b18f3e06227487efed7e209ee9e979928c95e0
selenefregosi-noaa <selene.fregosi@noaa.gov>
2025-01-23 08:55:21 -0800

    added output name for saving/titling

--- agate/utils/mapPlannedTrack.m
+++ agate/utils/mapPlannedTrack.m
@@ -1,8 +1,8 @@
-function mapPlannedTrack(CONFIG, targetsFile, varargin)
+function targetsName = mapPlannedTrack(CONFIG, targetsFile, varargin)
 %MAPPLANNEDTRACK Create static map of planned mission track
 %
 %   Syntax:
-%       MAPPLANNEDTRACK(CONFIG, TARGETSFILE, VARARGIN)
+%      TARGETSNAME = MAPPLANNEDTRACK(CONFIG, TARGETSFILE, VARARGIN)
 %
 %   Description:
 %       Create a static map of the planned mission track from an input
@@ -15,54 +15,58 @@ function mapPlannedTrack(CONFIG, targetsFile, varargin)
 %
 %   Inputs:
 %       CONFIG        [struct] mission/agate configuration variable.
-%                     Required fields: CONFIG.glider, CONFIG.mission, 
-%                     CONFIG.path.mission, CONFIG.map plotting section 
+%                     Required fields: CONFIG.glider, CONFIG.mission,
+%                     CONFIG.path.mission, CONFIG.map plotting section
 %       targetsFile   [char] fullpath to targets file
 %
 %       all varargins are specified using name-value pairs
 %                 e.g., 'bathy', 1, 'figNum', 12
 %
-%       trackName     [char] optional argument for the legend entry. If 
+%       trackName     [char] optional argument for the legend entry. If
 %                     empty will just say 'glider', e.g., 'sg639'
 %       bathy         optional argument for bathymetry plotting
 %	                  [double] Set to 1 to plot bathymetry or 0 to only
-%                     plot land. Default is 0. Will look for bathy file in 
-%                     CONFIG.map.bathyFile. 
-%                     [char] Path to the bathymetry file (if you want to 
+%                     plot land. Default is 0. Will look for bathy file in
+%                     CONFIG.map.bathyFile.
+%                     [char] Path to the bathymetry file (if you want to
 %                     use a different one than specified in CONFIG or it is
 %                     not specified in CONFIG
 %       col_track	  [char or RGB mat] optional color for the track e.g.,
 %                     [1 0.4 0] for orange or 'black'. Default is orange
-%       figNum        [double] optional argument defining figure number 
-%                     so it doesn't keep making new figs but refreshes 
+%       figNum        [double] optional argument defining figure number
+%                     so it doesn't keep making new figs but refreshes
 %                     existing figure
 %
 %   Outputs:
-%       none          creates figure
+%       targetsName   [char] filename of the input or selected targets
+%                     file; optional output can be useful for saving/titles
+%       -----         creates figure
 %
 %   Examples:
 %       % use specified targets file, plot bathymetry, specify name/color
 %       mapPlannedTrack(CONFIG, targetsFile, 'trackName', 'sg679', ...
 %            'bathy', 1, 'col_track', 'black')
-%       % to be prompted to select the targets file, plot bathy
-%       mapPlannedTrack(CONFIG, [], 'bathy', 1)
+%       % to be prompted to select the targets file, plot bathy, output
+%       % targetsName for the targets file selected
+%       targetsName = mapPlannedTrack(CONFIG, [], 'bathy', 1)
 %       % to use default track name 'glider', and default no bathymetry,
 %       use default color orange
 %       mapPlannedTrack(CONFIG, targetsFile)
 %
 %   See also   MAKETARGETSFILE
-% 
+%
 %   Authors:
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
-%   FirstVersion:   22 March 2023
-%   Updated:        19 September 2024
+%   Updated:      23 January 2025
 %
 %   Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % argument checks
 narginchk(2, inf)
+
+% check for nargout is below after checks for targetsFile
 
 % set defaults/empties
 trackName = 'glider';
@@ -73,39 +77,44 @@ figNum = [];
 % parse arguments
 vIdx = 1;
 while vIdx <= length(varargin)
-	switch varargin{vIdx}
-		case 'trackName'
-			trackName = varargin{vIdx+1};
-			vIdx = vIdx+2;
-		case 'bathy'
-			% just carry the arg through to createBasemap
-			bathy = varargin{vIdx+1};
-			vIdx = vIdx+2;
-		case 'col_track'
-			col_track = varargin{vIdx+1};
-			vIdx = vIdx+2;
-		case 'figNum'
-			figNum = varargin{vIdx+1};
-			vIdx = vIdx+2;
-		otherwise
-			error('Incorrect argument. Check inputs.');
-	end
+    switch varargin{vIdx}
+        case 'trackName'
+            trackName = varargin{vIdx+1};
+            vIdx = vIdx+2;
+        case 'bathy'
+            % just carry the arg through to createBasemap
+            bathy = varargin{vIdx+1};
+            vIdx = vIdx+2;
+        case 'col_track'
+            col_track = varargin{vIdx+1};
+            vIdx = vIdx+2;
+        case 'figNum'
+            figNum = varargin{vIdx+1};
+            vIdx = vIdx+2;
+        otherwise
+            error('Incorrect argument. Check inputs.');
+    end
 end
 
 if isempty(targetsFile) || ~exist(targetsFile, 'file')
     [fn, path] = uigetfile(fullfile(CONFIG.path.mission, '*.*'), ...
         'Select targets file');
     targetsFile = fullfile(path, fn);
+    fprintf('targets file selected: %s\n', fn);
 end
 
+% get file name for output argument for titling/saving
+if nargout > 0
+    [~, targetsName, ~] = fileparts(targetsFile);
+end
 
 % create basemap
 % by default, don't include contours. Include bathymetry if specified.
 [baseFig] = createBasemap(CONFIG, 'bathy', bathy, 'contourOn', 0, ...
-	'figNum', figNum);
+    'figNum', figNum);
 
 % plot glider track from targets file
-[targets, ~] = readTargetsFile(CONFIG, targetsFile); 
+[targets, ~] = readTargetsFile(CONFIG, targetsFile);
 
 plotm(targets.lat, targets.lon, 'Marker', 'o', 'MarkerSize', 4, 'MarkerEdgeColor', [0 0 0], ...
     'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], 'HandleVisibility', 'off')
@@ -117,7 +126,8 @@ h(1) = linem(targets.lat, targets.lon, 'LineWidth', 2, 'Color', col_track,...
 legend(h, {trackName}, 'Location', 'southeast', 'FontSize', 14)
 
 % add title
-title(sprintf('%s %s', CONFIG.glider, CONFIG.mission), 'Interpreter', 'none')
+title(sprintf('%s %s: %s', CONFIG.glider, CONFIG.mission, targetsName), ...
+    'Interpreter', 'none')
 
 end
 
