b1d328cef08f5d4285693487eafda39a45e9f630
selenefregosi-noaa <selene.fregosi@noaa.gov>
2025-07-15 11:56:31 -0700

    add conversion step, more details

--- agate/scratch/workflow_exportWisprSensitivity.m
+++ agate/scratch/workflow_exportWisprSensitivity.m
@@ -22,6 +22,45 @@
 % addpath(genpath('C:\Users\User.Name\Documents\MATLAB\agate'))
 addpath(genpath('C:\Users\selene.fregosi\Documents\MATLAB\agate'))
 
+
+%% convert to .wav or .flac if needed
+% this can be useful to browse through several files recorded to find one
+% with a complete calibration signal
+
+convertWispr;
+% this will prompt to select the raw file location and the output file 
+% location and will default to writing to flac. Alternatively, specify more
+% input arguments to avoid promts and choose wave output
+% convertWispr('inDir', 'E:/wisprFiles', 'outDir', 'E:/wav', 'outExt', '.wav')
+
+% identify a good candidate file
+% 134457
+
+%% Define some settings
+
+% set to empty to prompt/use defaults
+metadata = [];
+path_out = [];
+outType = 'both';   % 'netcdf' or 'csv' or 'both'
+fRange = [1 70000]; % this is the frequency that is actually usable
+
+
+
+sr = generateWisprSystemSensitivity(metadata, path_out, outType, fRange);
+
+%% Test with WISPR2
+
+metadata = [];
+path_out = [];
+outType = 'both';
+fRange = [1 70000];
+
+
+
+sr = generateWisprSystemSensitivity(metadata, path_out, outType, fRange);
+
+%% Test with WISPR3
+
 metadata = [];
 path_out = [];
 outType = 'both';
