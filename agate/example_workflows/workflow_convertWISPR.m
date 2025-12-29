4f7242bd28b3b2c80ddc7fdf26d88c3e1f84ccc9
selenefregosi-noaa <188515456+sfregosi-noaa@users.noreply.github.com>
2025-12-29 15:57:47 -0800

    generalized example, more checks to function

--- agate/example_workflows/workflow_convertWISPR.m
+++ agate/example_workflows/workflow_convertWISPR.m
@@ -4,16 +4,27 @@
 %	Description:
 %		This script provides the some example steps to convert a directory
 %		(or directories) of raw WISPR .dat files into a more readable
-%		format, either .flac or .wav. Each section of the script is an
-%		alternative approach with different input arguments. 
-%
-%       The user will need to update the path to agate. Optionallly, the
-%       user can specify the name/location of a mission configuration file
-%       that contains CONIFG.ws.inDir and CONFIG.ws.outDir to define the
-%       paths to the input directory of raw files and the target output
-%       directory, respectively. If a configuration file is not specified
-%       (or a blank one is used), the convertWispr function will prompt the
-%       user to select the desired folders. 
+%		format, either .flac or .wav. Regardless of which example is used,
+%		the user will need to update the path to agate in the first line.
+% 
+%       The first section provides three example uses where no agate
+%       mission configuration file is needed. This is useful for
+%       non-Seaglider based missions (Hefring Oceanscout or TWR Slocum) or
+%       for quick conversions of small test datasets. As few or as many 
+%       input arguments can be specified. 
+% 
+%       The first section provides examples if the user has a mission
+%       configuration file and is useful for batch processing after a full
+%       mission to avoid manual selection of directories and settings. 
+%       First, agate is initialized using this mission configuration file 
+%       that contains CONIFG.ws.inDir, CONFIG.ws.outDir, and/or 
+%       CONFIG.ws.outExt to define the paths to the input directory of raw 
+%       files, the target output directory, and the desired output format 
+%       ('.flac' or '.wav') respectively. If a configuration file is not 
+%       specified, a blank config file is provided, or either of those
+%       configuration settings are not specified in the file, the
+%       convertWispr function will prompt the user to select the desired
+%       folders and will default to .flac output format. 
 %
 %       Currently, the input directory must either be a directory named
 %       with the date or a directory full of subdirectories where each
@@ -36,12 +47,23 @@
 % add agate to the path
 addpath(genpath('C:\Users\User.Name\Documents\MATLAB\agate'))
 
-%% Using a configuration file and all defaults
+%% 1 - No mission configuration file needed
 
-% initialize agate
-% this can be empty but setting CONFIG.ws.inDir and CONFIG.ws.outDir will
-% streamline processing so the directories do not need to be manually
-% selected
+% 1a - No input arguments will prompt for directories, write to .flac
+convertWispr;
+
+% 1b - Write to .wav and do not print progress, will prompt for directories
+convertWispr('outExt', '.wav', 'showProgress', false)
+
+% 1c - Specify paths without CONFIG file, will write to .flac (default)
+convertWispr('inDir', 'C:\raw_files', 'outDir', 'C:\flac_files')
+
+%% 1 - Using a configuration file and all defaults
+
+% initialize agate with a configuration file
+% this can be empty but setting CONFIG.ws.inDir and CONFIG.ws.outDir
+% streamlines processing so directories do not need to be manually selected
+% in the event of a restart
 CONFIG = agate('agate_mission_config.cnf');
 
 % process all the files!
@@ -55,18 +77,3 @@ convertWispr(CONFIG);
 % by date with the format 'YYMMDD', so enter the name of the dated 
 % subdirectory as a 6 digit string to restart there
 convertWispr(CONFIG, 'restartDir', '240919');
-
-%% No configuration file will prompt for directories, write to .flac
-
-% write to default .flac
-convertWispr;
-
-%% Write to .wav and do not print progress
-
-% will prompt to select directories
-convertWispr('outExt', '.wav', 'showProgress', false)
-
-%% Specify paths without CONFIG file
-
-% will write default .flac and show progress
-convertWispr('inDir', 'C:\raw_files', 'outDir', 'C:\flac_files')
