0a35b6fc60d07668aceaf99b00cc9fc349e0e314
selenefregosi-noaa <188515456+sfregosi-noaa@users.noreply.github.com>
2026-03-10 07:25:46 -0700

    indentation changes

--- agate/utils/extractPilotingParams.m
+++ agate/utils/extractPilotingParams.m
@@ -48,7 +48,7 @@ function pp = extractPilotingParams(CONFIG, path_bsLocal, path_status, preload)
 %   Authors:
 %       S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
 %
-%   Updated:        2025 July 07
+%   Updated:        2026 March 10
 %
 %   Created with MATLAB ver.: 9.13.0.2166757 (R2022b) Update 4
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -282,214 +282,214 @@ for d = loopNums
     pp.('INTERNAL_PRESSURE')(d) = vals(2);
 
 
-	%% pmar outputs
-	if isfield(CONFIG, 'pm') && CONFIG.pm.loggers == 1 % pam system is active
-		% operating duration
-		idx = strfind(x, '$SENSOR_SECS');
-		idxComma = regexp(x(idx:end), '\,');
-		sVal = str2double(x(idx+idxComma(7):idx+idxComma(8)-2));
-		pp.PMAR_SEC(d) = sVal;
-		pp.PMAR_MIN(d) = sVal/60;
+    %% pmar outputs
+    if isfield(CONFIG, 'pm') && CONFIG.pm.loggers == 1 % pam system is active
+        % operating duration
+        idx = strfind(x, '$SENSOR_SECS');
+        idxComma = regexp(x(idx:end), '\,');
+        sVal = str2double(x(idx+idxComma(7):idx+idxComma(8)-2));
+        pp.PMAR_SEC(d) = sVal;
+        pp.PMAR_MIN(d) = sVal/60;
 
-		% power draw
-		idx = strfind(x, '$SENSOR_MAMPS');
-		idxComma = regexp(x(idx:end), '\,');
-		sVal = str2double(x(idx+idxComma(7):idx+idxComma(8)-2));
-		pp.PMAR_MAMPS(d) = sVal;
+        % power draw
+        idx = strfind(x, '$SENSOR_MAMPS');
+        idxComma = regexp(x(idx:end), '\,');
+        sVal = str2double(x(idx+idxComma(7):idx+idxComma(8)-2));
+        pp.PMAR_MAMPS(d) = sVal;
 
-		% kJ used ***EXPERIMENTAL***
-		pp.PMAR_kJ(d) = calckJ(pp.PMAR_SEC(d), pp.PMAR_MAMPS(d), 15);
+        % kJ used ***EXPERIMENTAL***
+        pp.PMAR_kJ(d) = calckJ(pp.PMAR_SEC(d), pp.PMAR_MAMPS(d), 15);
 
-		% find the current card
-		if CONFIG.sgVer == 66.12 % active card is listed in log file
-			activeCard = str2double(parseLogToBreak(x, '$PM_ACTIVECARD'));
-			activeCardStr = sprintf('0%d', activeCard);
-			pp.activeCard(d) = activeCard;
-		elseif CONFIG.sgVer == 67.00
-			% for sgVer 67.00 does it not print the active card - must specify
-			% in CONFIG
-			if isfield(CONFIG.pm,'activeCard')
-				% which active card for this dive?
-				acIdx = find(d >= CONFIG.pm.activeCard(:,1), 1, 'last');
-				pp.activeCard(d) = CONFIG.pm.activeCard(acIdx,2);
-				activeCardStr = sprintf('0%d', CONFIG.pm.activeCard(acIdx,2));
-			else
-				fprintf(1, 'Need to specify CONFIG.pm.activeCard. Exiting.\n')
-				return
-			end
-		end
+        % find the current card
+        if CONFIG.sgVer == 66.12 % active card is listed in log file
+            activeCard = str2double(parseLogToBreak(x, '$PM_ACTIVECARD'));
+            activeCardStr = sprintf('0%d', activeCard);
+            pp.activeCard(d) = activeCard;
+        elseif CONFIG.sgVer == 67.00
+            % for sgVer 67.00 does it not print the active card - must specify
+            % in CONFIG
+            if isfield(CONFIG.pm,'activeCard')
+                % which active card for this dive?
+                acIdx = find(d >= CONFIG.pm.activeCard(:,1), 1, 'last');
+                pp.activeCard(d) = CONFIG.pm.activeCard(acIdx,2);
+                activeCardStr = sprintf('0%d', CONFIG.pm.activeCard(acIdx,2));
+            else
+                fprintf(1, 'Need to specify CONFIG.pm.activeCard. Exiting.\n')
+                return
+            end
+        end
 
-		% number of files recorded per dive
-		paFolders = dir(fullfile(path_bsLocal, ['pm' num2str(d,'%04.f') '*']));
+        % number of files recorded per dive
+        paFolders = dir(fullfile(path_bsLocal, ['pm' num2str(d,'%04.f') '*']));
 
-		for pf = 1:length(paFolders)
-			xp = fileread(fullfile(path_bsLocal, paFolders(pf).name, ...
-				'pm_ch00.eng'));
-			val = str2double(parseLogToBreak(xp, '%datafiles:'));
-			if strcmp(paFolders(pf).name(end), 'a')
-				pp.numFilesDive(d) = val;
-			elseif strcmp(paFolders(pf).name(end), 'b')
-				pp.numFilesClimb(d) = val;
-			end
-		end
-		pp.numFiles(d) = sum([pp.numFilesDive(d) pp.numFilesClimb(d)], ...
-			'omitnan');
+        for pf = 1:length(paFolders)
+            xp = fileread(fullfile(path_bsLocal, paFolders(pf).name, ...
+                'pm_ch00.eng'));
+            val = str2double(parseLogToBreak(xp, '%datafiles:'));
+            if strcmp(paFolders(pf).name(end), 'a')
+                pp.numFilesDive(d) = val;
+            elseif strcmp(paFolders(pf).name(end), 'b')
+                pp.numFilesClimb(d) = val;
+            end
+        end
+        pp.numFiles(d) = sum([pp.numFilesDive(d) pp.numFilesClimb(d)], ...
+            'omitnan');
 
-		% space used in that dive
-		% first set up this column (Want it to come before by-card space
-		if d == 1
-			pp.pmUsed_GB(d,1) = NaN;
-		end
+        % space used in that dive
+        % first set up this column (Want it to come before by-card space
+        if d == 1
+            pp.pmUsed_GB(d,1) = NaN;
+        end
 
-		% free space per card
-		for sdc = 1:CONFIG.pm.numCards
-			cardNumStr = sprintf('0%d', sdc-1);
-			val = str2double(parseLogToBreak(x,  ['$PM_FREEKB_' cardNumStr]));
-			if ~isempty(val)
-				pp.(['pmFree_' cardNumStr '_GB'])(d) = val/1000000;
-			else
-				pp.(['pmFree_' cardNumStr '_GB'])(d) = NaN;
-			end
-		end
+        % free space per card
+        for sdc = 1:CONFIG.pm.numCards
+            cardNumStr = sprintf('0%d', sdc-1);
+            val = str2double(parseLogToBreak(x,  ['$PM_FREEKB_' cardNumStr]));
+            if ~isempty(val)
+                pp.(['pmFree_' cardNumStr '_GB'])(d) = val/1000000;
+            else
+                pp.(['pmFree_' cardNumStr '_GB'])(d) = NaN;
+            end
+        end
 
-		% space used on this dive (if not Dive 1)
-		if d > 1
-			pp.pmUsed_GB(d,1) = (pp.(['pmFree_' activeCardStr '_GB'])(d-1) - ...
-				pp.(['pmFree_' activeCardStr '_GB'])(d));
-		end
-	end
+        % space used on this dive (if not Dive 1)
+        if d > 1
+            pp.pmUsed_GB(d,1) = (pp.(['pmFree_' activeCardStr '_GB'])(d-1) - ...
+                pp.(['pmFree_' activeCardStr '_GB'])(d));
+        end
+    end
 
-	%% wispr outputs
-	% 	if isfield(CONFIG, 'ws') && CONFIG.ws.loggers == 1 % wispr pam system is active
-	%
-	% 		% WISPR reported analysis and processing durations
-	% 		% for now, only reading 'a' files from glider descent; 'b' files all blank
-	% 		wsFiles = dir(fullfile(path_bsLocal, ['ws' num2str(d, '%04.f') 'az']));
-	% 		bytes = [wsFiles.bytes];
-	% 		wsFiles = wsFiles(bytes > 2,:);
-	% 		for wf = 1:length(wsFiles)
-	% 			ws = readws(fullfile(path_bsLocal, wsFiles(wf).name));
-	% 		end
-	% 		pp.wsAnDurDive_min(d) = ws.anDur_min;
-	% 		pp.wsProcTimeDive_sec(d) = ws.procTime_sec;
-	% 		pp.wsAnDurClimb_min(d) = NaN;
-	% 		pp.wsProcTimeClimb_sec(d) = NaN;
-	%
-	% 		% seaglider reported operating duration
-	% 		idx = strfind(x, '$SENSOR_SECS');
-	% 		idxComma = regexp(x(idx:end), '\,');
-	% 		sVal = round(str2double(x(idx+idxComma(8):idx+idxComma(9)-2)));
-	% 		pp.WS_SEC(d) = sVal;
-	% 		pp.WS_MIN(d) = sVal/60;
-	%
-	% 		% power draw
-	% 		%         idx = strfind(x, '$SENSOR_MAMPS');
-	% 		%         idxComma = regexp(x(idx:end), '\,');
-	% 		%         sVal = str2double(x(idx+idxComma(8):idx+idxComma(9)-2));
-	% 		%         pp.WS_MAMPS(d) = sVal;
-	% 		WS_MAMPS = 33.3; % estimate 0.5 W @ 15 v = 33.3 mAmps
-	%
-	% 		% seaglider on board battery is not accounting for correct WISPR
-	% 		% power draw and RPi/detector power draw
-	%
-	% 		% kJ used ***EXPERIMENTAL***
-	% 		% get base kJ just used by WISPR
-	% 		kJ_base = calckJ(pp.WS_SEC(d), WS_MAMPS, 15);
-	% 		% then get kJ used by RPi...this is very rough estimate
-	% 		RPi_MAMPS = 400; % estimate 6 W @ 15 V = 400 mAmps
-	% 		RPi_SECS = pp.wsProcTimeDive_sec(d)*2 + 10; % from ws****az file,
-	% 		% x2 because only dive reported,
-	% 		% + 10 sec for start up
-	% 		kJ_RPi = calckJ(RPi_SECS, RPi_MAMPS, 15);
-	% 		pp.WS_kJ(d) = kJ_base + kJ_RPi;
-	%
-	% 		% calculate a WISPR ampHr to be added to the cumulative
-	% 		% ampHrConsumed value output in the .log file below
-	% 		Ah_base = calcAh(pp.WS_SEC(d), WS_MAMPS);
-	% 		Ah_RPi = calcAh(RPi_SECS, RPi_MAMPS);
-	%
-	% 		% seaglider on board battery is not accounting for correct WISPR
-	% 		% power draw and RPi/detector power draw
-	% 		% calculate a WISPR ampHr to be added to the cumulative
-	% 		% ampHrConsumed value output in the .log file below
-	% 		pp.WS_ampHr(d) = Ah_base + Ah_RPi;
-	% 		% could also calculate this from total kJ
-	% 		%         pp.WS_ampHr(d) = pp.WS_kJ(d)/3.6/15;
-	%
-	% 	end
+    %% wispr outputs
+    % 	if isfield(CONFIG, 'ws') && CONFIG.ws.loggers == 1 % wispr pam system is active
+    %
+    % 		% WISPR reported analysis and processing durations
+    % 		% for now, only reading 'a' files from glider descent; 'b' files all blank
+    % 		wsFiles = dir(fullfile(path_bsLocal, ['ws' num2str(d, '%04.f') 'az']));
+    % 		bytes = [wsFiles.bytes];
+    % 		wsFiles = wsFiles(bytes > 2,:);
+    % 		for wf = 1:length(wsFiles)
+    % 			ws = readws(fullfile(path_bsLocal, wsFiles(wf).name));
+    % 		end
+    % 		pp.wsAnDurDive_min(d) = ws.anDur_min;
+    % 		pp.wsProcTimeDive_sec(d) = ws.procTime_sec;
+    % 		pp.wsAnDurClimb_min(d) = NaN;
+    % 		pp.wsProcTimeClimb_sec(d) = NaN;
+    %
+    % 		% seaglider reported operating duration
+    % 		idx = strfind(x, '$SENSOR_SECS');
+    % 		idxComma = regexp(x(idx:end), '\,');
+    % 		sVal = round(str2double(x(idx+idxComma(8):idx+idxComma(9)-2)));
+    % 		pp.WS_SEC(d) = sVal;
+    % 		pp.WS_MIN(d) = sVal/60;
+    %
+    % 		% power draw
+    % 		%         idx = strfind(x, '$SENSOR_MAMPS');
+    % 		%         idxComma = regexp(x(idx:end), '\,');
+    % 		%         sVal = str2double(x(idx+idxComma(8):idx+idxComma(9)-2));
+    % 		%         pp.WS_MAMPS(d) = sVal;
+    % 		WS_MAMPS = 33.3; % estimate 0.5 W @ 15 v = 33.3 mAmps
+    %
+    % 		% seaglider on board battery is not accounting for correct WISPR
+    % 		% power draw and RPi/detector power draw
+    %
+    % 		% kJ used ***EXPERIMENTAL***
+    % 		% get base kJ just used by WISPR
+    % 		kJ_base = calckJ(pp.WS_SEC(d), WS_MAMPS, 15);
+    % 		% then get kJ used by RPi...this is very rough estimate
+    % 		RPi_MAMPS = 400; % estimate 6 W @ 15 V = 400 mAmps
+    % 		RPi_SECS = pp.wsProcTimeDive_sec(d)*2 + 10; % from ws****az file,
+    % 		% x2 because only dive reported,
+    % 		% + 10 sec for start up
+    % 		kJ_RPi = calckJ(RPi_SECS, RPi_MAMPS, 15);
+    % 		pp.WS_kJ(d) = kJ_base + kJ_RPi;
+    %
+    % 		% calculate a WISPR ampHr to be added to the cumulative
+    % 		% ampHrConsumed value output in the .log file below
+    % 		Ah_base = calcAh(pp.WS_SEC(d), WS_MAMPS);
+    % 		Ah_RPi = calcAh(RPi_SECS, RPi_MAMPS);
+    %
+    % 		% seaglider on board battery is not accounting for correct WISPR
+    % 		% power draw and RPi/detector power draw
+    % 		% calculate a WISPR ampHr to be added to the cumulative
+    % 		% ampHrConsumed value output in the .log file below
+    % 		pp.WS_ampHr(d) = Ah_base + Ah_RPi;
+    % 		% could also calculate this from total kJ
+    % 		%         pp.WS_ampHr(d) = pp.WS_kJ(d)/3.6/15;
+    %
+    % 	end
 
 
-	%% battery usage
-	% voltages
-	idx = strfind(x, '$24V_AH');
-	sl = length('$24V_AH');
-	idxComma = regexp(x(idx:end), '\,');
-	idxBreak = regexp(x(idx+sl+1:end),'\n','once') + idx + sl;
-	sVal = str2double(x(idx+idxComma(2):idxBreak-1));
-	pp.ampHrConsumed(d) = sVal;
+    %% battery usage
+    % voltages
+    idx = strfind(x, '$24V_AH');
+    sl = length('$24V_AH');
+    idxComma = regexp(x(idx:end), '\,');
+    idxBreak = regexp(x(idx+sl+1:end),'\n','once') + idx + sl;
+    sVal = str2double(x(idx+idxComma(2):idxBreak-1));
+    pp.ampHrConsumed(d) = sVal;
 
-	% minimum voltages
-	sVal = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
-	pp.minVolt_24(d) = sVal;
-	idx = strfind(x, '$10V_AH');
-	idxComma = regexp(x(idx:end), '\,');
-	sVal = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
-	pp.minVolt_10(d) = sVal;
+    % minimum voltages
+    sVal = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
+    pp.minVolt_24(d) = sVal;
+    idx = strfind(x, '$10V_AH');
+    idxComma = regexp(x(idx:end), '\,');
+    sVal = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
+    pp.minVolt_10(d) = sVal;
 
-	% By Devices *****EXPERIMENTAL******
-	if CONFIG.sgVer == 66.12 || CONFIG.sgVer == 66.14
-		% order of devices = pitch, roll, VBD apogee, VBD surf, VBD valve
-		idx = strfind(x, '$DEVICE_SECS');
-		idxComma = regexp(x(idx:end), '\,');
-		pSec    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
-		rSec    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
-		vSec1   = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
-		vSec2   = str2double(x(idx+idxComma(4):idx+idxComma(5)-2));
-		vSec3   = str2double(x(idx+idxComma(5):idx+idxComma(6)-2));
+    % By Devices *****EXPERIMENTAL******
+    if CONFIG.sgVer == 66.12 || CONFIG.sgVer == 66.14
+        % order of devices = pitch, roll, VBD apogee, VBD surf, VBD valve
+        idx = strfind(x, '$DEVICE_SECS');
+        idxComma = regexp(x(idx:end), '\,');
+        pSec    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
+        rSec    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
+        vSec1   = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
+        vSec2   = str2double(x(idx+idxComma(4):idx+idxComma(5)-2));
+        vSec3   = str2double(x(idx+idxComma(5):idx+idxComma(6)-2));
 
-		idx = strfind(x, '$DEVICE_MAMPS');
-		idxComma = regexp(x(idx:end), '\,');
-		pMamps    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
-		rMamps    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
-		vMamps1   = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
-		vMamps2   = str2double(x(idx+idxComma(4):idx+idxComma(5)-2));
-		vMamps3   = str2double(x(idx+idxComma(5):idx+idxComma(6)-2));
+        idx = strfind(x, '$DEVICE_MAMPS');
+        idxComma = regexp(x(idx:end), '\,');
+        pMamps    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
+        rMamps    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
+        vMamps1   = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
+        vMamps2   = str2double(x(idx+idxComma(4):idx+idxComma(5)-2));
+        vMamps3   = str2double(x(idx+idxComma(5):idx+idxComma(6)-2));
 
-		V = 15;
-		pp.pkJ(d,1) = calckJ(pSec, pMamps, V);
-		pp.rkJ(d,1) = calckJ(rSec, rMamps, V);
-		vkJ1 = calckJ(vSec1, vMamps1, V);
-		vkJ2 = calckJ(vSec2, vMamps2, V);
-		vkJ3 = calckJ(vSec3, vMamps3, V);
-		pp.vkJ(d,1) = vkJ1 + vkJ2 + vkJ3;
+        V = 15;
+        pp.pkJ(d,1) = calckJ(pSec, pMamps, V);
+        pp.rkJ(d,1) = calckJ(rSec, rMamps, V);
+        vkJ1 = calckJ(vSec1, vMamps1, V);
+        vkJ2 = calckJ(vSec2, vMamps2, V);
+        vkJ3 = calckJ(vSec3, vMamps3, V);
+        pp.vkJ(d,1) = vkJ1 + vkJ2 + vkJ3;
 
-	elseif CONFIG.sgVer == 67.00 || CONFIG.sgVer == 67.01
-		% order of devices = VBD, pitch, roll
-		idx = strfind(x, '$DEVICE_SECS');
-		idxComma = regexp(x(idx:end), '\,');
-		vSec    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
-		pSec    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
-		rSec    = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
+    elseif CONFIG.sgVer == 67.00 || CONFIG.sgVer == 67.01
+        % order of devices = VBD, pitch, roll
+        idx = strfind(x, '$DEVICE_SECS');
+        idxComma = regexp(x(idx:end), '\,');
+        vSec    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
+        pSec    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
+        rSec    = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
 
-		idx = strfind(x, '$DEVICE_MAMPS');
-		idxComma = regexp(x(idx:end), '\,');
-		vMamps    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
-		pMamps    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
-		rMamps    = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
+        idx = strfind(x, '$DEVICE_MAMPS');
+        idxComma = regexp(x(idx:end), '\,');
+        vMamps    = str2double(x(idx+idxComma(1):idx+idxComma(2)-2));
+        pMamps    = str2double(x(idx+idxComma(2):idx+idxComma(3)-2));
+        rMamps    = str2double(x(idx+idxComma(3):idx+idxComma(4)-2));
 
-		pp.pkJ(d,1) = pSec*pMamps*15/1000000;
-		pp.rkJ(d,1) = rSec*rMamps*15/1000000;
-		pp.vkJ(d,1) = vSec*vMamps*15/1000000;
-	end
+        pp.pkJ(d,1) = pSec*pMamps*15/1000000;
+        pp.rkJ(d,1) = rSec*rMamps*15/1000000;
+        pp.vkJ(d,1) = vSec*vMamps*15/1000000;
+    end
 
-	%% depth average currents
-	if ~isempty(ncFileName)
-		pp.dac_east_cm_s(d) = ncread(ncFileName,'depth_avg_curr_east')*100;
-		pp.dac_north_cm_s(d) = ncread(ncFileName,'depth_avg_curr_north')*100;
-	end
+    %% depth average currents
+    if ~isempty(ncFileName)
+        pp.dac_east_cm_s(d) = ncread(ncFileName,'depth_avg_curr_east')*100;
+        pp.dac_north_cm_s(d) = ncread(ncFileName,'depth_avg_curr_north')*100;
+    end
 
-	%% any errors?
-	pp.ERRORS{d} = parseLogToBreak(x, '$ERRORS');
+    %% any errors?
+    pp.ERRORS{d} = parseLogToBreak(x, '$ERRORS');
 
 end % dives
 
